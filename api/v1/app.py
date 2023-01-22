5e604a9de7cc3758b9016d2027c03c9b673aecdf
Mubarek Seid <mubareksd@gmail.com>
2023-01-22 19:52:41 +0300

    task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -17,6 +17,13 @@ def teardown_app(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    """handle non existing pages
+    """
+    return make_response(jsonify({'error': "Not found"}), 404)
+
+
 if __name__ == '__main__':
     host = getenv("HBNB_API_HOST", "0.0.0.0")
     port = getenv("HBNB_API_PORT", "5000")
