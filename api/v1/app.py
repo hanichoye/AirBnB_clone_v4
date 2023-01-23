e3e02575dc4002a45777b77499f2ef2e21d414ca
Mubarek Seid <mubareksd@gmail.com>
2023-01-23 18:19:01 +0300

    update

--- api/v1/app.py
+++ api/v1/app.py
@@ -27,4 +27,5 @@ def not_found(error):
 if __name__ == '__main__':
     host = getenv("HBNB_API_HOST", "0.0.0.0")
     port = getenv("HBNB_API_PORT", "5000")
+    app.run(debug=True)
     app.run(host=host, port=port)
