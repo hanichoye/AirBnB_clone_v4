76682bbf0651af0bab777df6af1321a51f9ef30f
Mubarek Seid <mubareksd@gmail.com>
2023-01-23 18:53:42 +0300

    update

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
 """Comment
 """
-from api.v1.views import app_views
-from flask import (
-    abort,
-    jsonify,
-    make_response,
-    request
-)
-from models import storage
 from models.state import State
+from models import storage
+from api.v1.views import app_views
+from flask import abort, jsonify, make_response, request
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
